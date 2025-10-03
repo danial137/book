b9a2f3e269b463f41988438619740b41c67f5e41
danial137 <danial79fakhrabadi@gmail.com>
2025-10-03 15:21:09 +0330

    fix some bug

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from "react";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+const Layout = async ({ children }: { children: ReactNode }) => {
+    // const session = await auth();
+
+    // if (session) redirect("/");
+
+    return (
+        <main className="auth-container flex h-screen">
+
+            <section className="auth-form flex-1 flex items-center justify-center">
+                <div className="auth-box">
+                    <div className="flex flex-row gap-3 mb-6">
+                        <Image src="/icons/logo.svg" alt="logo" width={37} height={37} />
+                        <h1 className="text-2xl font-semibold text-white">BookWise</h1>
+                    </div>
+                    <div>{children}</div>
+                </div>
+            </section>
+
+
+            <section className="auth-illustration relative flex-1 h-full">
+                <Image
+                    src="/images/auth-illustration.png"
+                    alt="auth illustration"
+                    fill
+                    className="object-cover"
+                    priority
+                />
+            </section>
+        </main>
+    );
+};
+
+export default Layout;
--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import React from 'react'
 import BookCover from './BookCover'
 import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import { Button } from './ui/button'
 
 const BookCard = ({ id, title, genre, cover, isLoadingBook = false,
 
@@ -10,10 +12,20 @@ const BookCard = ({ id, title, genre, cover, isLoadingBook = false,
             <BookCover coverColor={cover} coverImage={cover} />
 
 
-            <div className={cn('mt-4',!isLoadingBook && "xs:max-w-40 max-w-28")}>
+            <div className={cn('mt-4', !isLoadingBook && "xs:max-w-40 max-w-28")}>
                 <p className='book-title'>{title}</p>
                 <p className='book-genre'>{genre}</p>
             </div>
+
+            {isLoadingBook && (
+                <div className='mt-3 w-full'>
+                    <div className='book-loaned'>
+                        <Image src='/icons/calendar.svg' height={18} width={18} alt='svg' className='object-contain' />
+                        <p className='text-light-100'>11 days left to return</p>
+                    </div>
+                    <Button className='book-btn'>Download receipt</Button>
+                </div>
+            )}
 
         </Link>
     </li>
